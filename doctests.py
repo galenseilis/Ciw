8a902d36e8a8cac242043b7897c08ea496ff8996
galenseilis <galen.seilis@seilis.ca>
2024-06-19 19:37:34 -0700

    updated doctest to run only on docs and ciw

--- doctests.py
+++ doctests.py
@@ -3,18 +3,20 @@ import os
 import unittest
 import sys
 
+paths = ['./docs', './ciw']
 
 def load_tests(loader, tests, ignorex):
-    for root, dirs, files in os.walk("."):
-        for f in files:
-            if f.endswith(".rst"):
-                tests.addTests(
-                    doctest.DocFileSuite(
-                        os.path.join(root, f), optionflags=doctest.ELLIPSIS
-                    )
-                )
+	for path in paths:
+		for root, dirs, files in os.walk(path):
+			for f in files:
+				if f.endswith(".rst"):
+					tests.addTests(
+						doctest.DocFileSuite(
+							os.path.join(root, f), optionflags=doctest.ELLIPSIS
+						)
+					)
 
-    return tests
+		return tests
 
 
 if __name__ == "__main__":
